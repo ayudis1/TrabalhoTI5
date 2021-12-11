7eb07035ee969f11954ff1ea5fb58411ad285153
ayudis1 <artur.yudi@live.com>
2021-12-11 11:52:34 -0300

    teste

--- src/application/Main.java
+++ src/application/Main.java
@@ -16,7 +16,6 @@ public class Main extends Application {
 			primaryStage.setTitle("Tela Principal");
 			Parent root = FXMLLoader.load(getClass().getResource("mainFrame.fxml"));
 			Scene scene = new Scene(root,480,720);
-			//scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
